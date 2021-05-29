e3a97959fda512f60c6bd14de93377a41d7bb36f
nathalieutstumo <nathalie.utstumo@gmail.com>
2021-05-29 16:30:31 +0200

    Hotjar

--- js/details.js
+++ js/details.js
@@ -5,34 +5,30 @@ const params = new URLSearchParams(queryString);
 const id = params.get('id');
 
 async function getPosts() {
-try {
-console.log(id)
-const response = await fetch('https://projectexam.ntoni.tech/wp-json/wp/v2/posts/' + id);
-const jsonResults = await response.json();
-const jsonArray = jsonResults;
-console.log (jsonResults);
-console.log (jsonArray.title.rendered);
+    try {
+        console.log(id)
+        const response = await fetch('https://projectexam.ntoni.tech/wp-json/wp/v2/posts/' + id);
+        const jsonResult = await response.json();
+        console.log (jsonResult);
 
-for (let i = 0; i < jsonArray.length; i++) {
-    document.title = jsonArray[i].title.rendered;
-    document.querySelector('main').innerHTML += `
-    <div class="post">
-    <h1>${jsonArray[i].title.rendered}</h1>
-    <img class ="postImg" src="${jsonArray[i].fi_300x180}">
-    <p>${jsonArray[i].content.rendered}</p>
-</div>
-`;
-}
-} catch (error) {
-    console.log(error)
-    document.querySelector('.alert').innerHTML = showAlertTouser(
-        'error',
-        'danger'
-        );
+        document.title = jsonResult.title.rendered;
+        document.querySelector('main').innerHTML += `
+            <div class="post">
+                <h1>${jsonResult.title.rendered}</h1>
+                <img class ="detailsImg" src="${jsonResult.fi_300x180}">
+                <p>${jsonResult.content.rendered}</p>
+            </div>`;
+        
+    } catch (error) {
+        console.log(error)
+        document.querySelector('.alert').innerHTML = showAlertTouser(
+            'error',
+            'danger'
+            );
     }
 
 };  
-
 getPosts();
+
 
 {/* <h1>${jsonArray[i].title.rendered}</h1> */}
